84ea9842a1950d439e0578213dea06f26e6a201c
Rafael Alessandro <rafa.alessandro@gmail.com>
2020-04-05 14:54:45 -0300

    Ajustes no acesso ao banco de dados

--- src/test/java/com/eventosapp/eventosapp/EventosappApplicationTests.java
+++ src/test/java/com/eventosapp/eventosapp/EventosappApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EventosappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
